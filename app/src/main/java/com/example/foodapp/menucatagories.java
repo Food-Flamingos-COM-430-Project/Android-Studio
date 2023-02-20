e554305c384bd3823635c00706a93fbd0a95b57c
nsayles <noah.sayles@saintleo.edu>
2023-02-20 08:55:54 -0500

    step 3

--- app/src/main/java/com/example/foodapp/menucatagories.java
+++ app/src/main/java/com/example/foodapp/menucatagories.java
@@ -1,31 +1,27 @@
 package com.example.foodapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
+
 public class menucatagories extends Activity {
     private Button btnSubmit;
-    private boolean Italian = true;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate (@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menucatagories);
-
-        btnSubmit = findViewById(R.id.btnSubmit);
-        txtLoginInfo.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                if (Italian) {
-                    Italian = true;
-                    btnSubmit.setText("Italian");
-                }
-
-
-        }
-
-
     }
+    private void onClick(View view){
+        boolean italian = true;
+        if (italian){
+            italian = true;
+            btnSubmit.setText("Italian");
+            startActivity(new Intent(menucatagories.this,page1.class));
+        }
+    }
+
 }
