5fd06e3712356b8bb26d7ca68506589ba14ef9a5
nsayles <noah.sayles@saintleo.edu>
2023-02-20 21:04:12 -0500

    step 4

--- app/src/main/java/com/example/foodapp/menucatagories.java
+++ app/src/main/java/com/example/foodapp/menucatagories.java
@@ -1,27 +1,33 @@
 package com.example.foodapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class menucatagories extends Activity {
-    private Button btnSubmit;
+public class menucatagories extends AppCompatActivity {
+private Button btnSubmit;
     @Override
-    protected void onCreate (@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menucatagories);
-    }
-    private void onClick(View view){
-        boolean Italian = true;
-        if (Italian){
-            Italian = true;
-            btnSubmit.setText("Italian");
-            startActivity(new Intent(menucatagories.this,page1.class));
-        }
-    }
 
+        Button Italian = findViewById(R.id.btnSubmit6);
+        Italian.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(menucatagories.this, page1.class));
+            }
+        });
+        Button Kids = findViewById(R.id.btnSubmit7);
+        Kids.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(menucatagories.this, page2.class));
+            }
+
+        });
+    }
 }
+
--- app/src/main/java/com/example/foodapp/page1.java
+++ app/src/main/java/com/example/foodapp/page1.java
@@ -1,14 +1,27 @@
 package com.example.foodapp;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class page1 extends Activity {
-            @Override
-            protected void onCreate (@Nullable Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.page1);
+public class page1 extends AppCompatActivity {
+    private Button btnReturn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page1);
+
+        Button Return = findViewById(R.id.btnReturn);
+        Return.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(page1.this, menucatagories.class));
+            }
+        });
     }
 }
+
