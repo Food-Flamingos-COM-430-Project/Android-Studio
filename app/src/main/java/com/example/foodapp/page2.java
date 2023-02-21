e258446aa03eeebfeca9792a1f28a62cb721d53d
nsayles <noah.sayles@saintleo.edu>
2023-02-21 07:47:46 -0500

    step 4.1

--- app/src/main/java/com/example/foodapp/page2.java
+++ app/src/main/java/com/example/foodapp/page2.java
@@ -1,14 +1,26 @@
 package com.example.foodapp;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class page2 extends Activity {
+public class page2 extends AppCompatActivity {
+    private Button btnSubmit;
+
     @Override
-    protected void onCreate (@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page2);
+
+        Button Return = findViewById(R.id.btnReturn);
+        Return.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(page2.this, menucatagories.class));
+            }
+        });
     }
 }
